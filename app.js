9a3b1b0bb0e79671d669334ce31d59c1a80c9561
Lucas Corrêa <lucascorreagoldani04@gmail.com>
2022-08-20 13:26:10 -0300

    dateContiner create

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const clockContainer = document.querySelector('.clock-container')
+const dateContainer = document.querySelector('.date-container')
+
+const date = new Date()
+    const day = String(date.getDate()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = date.getFullYear()
+
+    const dateHTML = `<span>${day}/${month}/${year}</span>`
+
+    dateContainer.innerHTML = dateHTML
+
 
 const updateClock = () => {
     const present = new Date()
